9550355cfa3a69bf5bc814ec1fdaa89e765b8ae8
Arash <arash.mh01@gmail.com>
2019-01-17 11:02:47 -0800

    Complete build move tree

--- KnightsTravails/knightPathFinder.rb
+++ KnightsTravails/knightPathFinder.rb
@@ -29,29 +29,32 @@ class KnightPathFinder
         
         start_pos = @root_node
         bfs_queue = [start_pos]
-        
+        move_tree = []
         until bfs_queue.empty? 
             first_node = bfs_queue.shift
-            p first_node
-            # byebug
+            move_tree << first_node
             one_moves_away = new_move_positions(first_node)
             one_moves_away.each do |pos|
                 bfs_queue << PolyTreeNode.new(pos)
             end
         end
-            
+        move_tree
     end
 
     def new_move_positions(node)
         pos = node.value
         valid_move = KnightPathFinder.valid_moves(pos)
         arr = valid_move.select {|ele| !@considered_positions.include?(ele)}
-        return @considered_positions += arr 
+        @considered_positions += arr 
+        return arr
     end
 
+    def find_path(end_pos)
+        
+    end
     
 
     
 end
 
-k = KnightPathFinder.new([4,4])
+# k = KnightPathFinder.new([4,4])
