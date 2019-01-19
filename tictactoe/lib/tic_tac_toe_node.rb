78cd159e89808c4ebc0e4cd8b1bfb1c2f69f0a91
Arash <arash.mh01@gmail.com>
2019-01-19 15:03:59 -0800

    Complete children method

--- tictactoe/lib/tic_tac_toe_node.rb
+++ tictactoe/lib/tic_tac_toe_node.rb
@@ -17,49 +17,27 @@ class TicTacToeNode
   def winning_node?(evaluator)
   end
 
-  # This method generates an array of all moves 
-  # that can be made after
-  # the current move.
-  #Write a method children that returns nodes representing
-  # all the potential game states one move after the 
-  #current node. To create this method, it will be 
-  #necessary to iterate through all positions that are 
-  #empty? on the board object. For each empty position, 
-  #create a node by duping the board and putting 
-  #a next_mover_mark in the position. You'll want 
-  #to alternate next_mover_mark so that next time the 
-  #other player gets to move. Also, 
-  #set prev_move_pos to the position you just marked, 
-  #for reasons that will make sense when we use it later.
   def children
     children = []
-    # byebug
     mark = :x
     if @next_mover_mark == :x 
       mark = :o
     end
-    # byebug
     board_rows = @board.rows
     board_rows.each_with_index do |row, i|
       row.each_with_index do |ele, j|
         pos = [i, j]
-        
-        # byebug
         if @board.empty?(pos)
-          
-          @prev_move_pos = pos
-          # byebug
-          children << TicTacToeNode.new(@board.dup, mark)
+          children << TicTacToeNode.new(@board.dup, mark, pos)
         end
       end
     end
-    children
-    # byebug    
+    children  
   end
 
 end
 
-board = Board.new
-mark = :x
-node = TicTacToeNode.new(board, mark)
-node.children
+# board = Board.new
+# mark = :x
+# node = TicTacToeNode.new(board, mark)
+# node.children
