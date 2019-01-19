13388685da261218aaf645b5eadad8b551f4cca5
Arash <arash.mh01@gmail.com>
2019-01-19 14:27:06 -0800

    Add attrubute reader

--- tictactoe/lib/tic_tac_toe_node.rb
+++ tictactoe/lib/tic_tac_toe_node.rb
@@ -1,10 +1,14 @@
 require_relative 'tic_tac_toe'
+require "byebug"
 
 class TicTacToeNode
+  
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
     @prev_move_pos = prev_move_pos
+    
   end
 
   def losing_node?(evaluator)
@@ -13,8 +17,49 @@ class TicTacToeNode
   def winning_node?(evaluator)
   end
 
-  # This method generates an array of all moves that can be made after
+  # This method generates an array of all moves 
+  # that can be made after
   # the current move.
+  #Write a method children that returns nodes representing
+  # all the potential game states one move after the 
+  #current node. To create this method, it will be 
+  #necessary to iterate through all positions that are 
+  #empty? on the board object. For each empty position, 
+  #create a node by duping the board and putting 
+  #a next_mover_mark in the position. You'll want 
+  #to alternate next_mover_mark so that next time the 
+  #other player gets to move. Also, 
+  #set prev_move_pos to the position you just marked, 
+  #for reasons that will make sense when we use it later.
   def children
+    children = []
+    # byebug
+    mark = :x
+    if @next_mover_mark == :x 
+      mark = :o
+    end
+    # byebug
+    board_rows = @board.rows
+    board_rows.each_with_index do |row, i|
+      row.each_with_index do |ele, j|
+        pos = [i, j]
+        
+        # byebug
+        if @board.empty?(pos)
+          
+          @prev_move_pos = pos
+          # byebug
+          children << TicTacToeNode.new(@board.dup, mark)
+        end
+      end
+    end
+    children
+    # byebug    
   end
+
 end
+
+board = Board.new
+mark = :x
+node = TicTacToeNode.new(board, mark)
+node.children
