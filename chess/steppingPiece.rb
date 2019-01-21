606342dfb15abcfb95834de7628de153175b9d43
Arash <arash.mh01@gmail.com>
2019-01-21 14:08:09 -0800

    Complete move method in stepping pieces module

--- chess/steppingPiece.rb
+++ chess/steppingPiece.rb
@@ -2,6 +2,10 @@ module SteppingPiece
   
   def move
     moves = []
+    move_diffs.each do |x, y|
+      moves.concat(move_steps(x,y))
+    end
+    moves
   end
 
   def move_diffs
@@ -10,15 +14,19 @@ module SteppingPiece
 
   def move_steps(dx, dy)
     curr_x, curr_y = position
+    moves = []
+    
+    curr_x, curr_y = curr_x+dx, curr_y+dy
+    position = [curr_x, curr_y]
 
-    loop do
-      curr_x, curr_y = curr_x+dx, curr_y+dy
-      position = [curr_x, curr_y]
+    break until board.valid_pos?(position)
 
-      break until board.valid_pos?(position)
-
-      
+    if board[position] == nil
+      moves << position
+    else
+        moves << position if board[position].color != color
     end
+    moves
   end
 
 end
