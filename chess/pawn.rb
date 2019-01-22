8e0e73c6a4320f1bce5c678b66ed665acb2f3fc6
Arash <arash.mh01@gmail.com>
2019-01-22 12:17:44 -0800

    Add attack and forward move to pawn

--- chess/pawn.rb
+++ chess/pawn.rb
@@ -8,7 +8,47 @@ class Pawn < Piece
   end
 
   def moves
-    
+    moves = []
+    dx, dy = move_dirs
   end
+
+  def move_dirs
+    self.color == :blue ? [[-1,0], [-2,0]] : [[1,0], [2,0]]
+  end
+
+  def side_attacks
+    self.color == :blue ? [[-1,1], [-1,-1]] : [[1,-1], [1,1]]
+  end
+
+  def move_forward_not_blocked(dx, dy)
+    moves = []
+    x, y = self.position
+    x, y = x+dx, y+dy
+    new_position = [x, y]
+    if dx.abs != 2
+      if @board.empty?(new_position) && @board.valid_position?(new_position)
+        moves << new_position
+        position = new_position
+      end
+    else
+      if @board.empty?(new_position) && (self.position[0] == 1 || self.position[0] == 6)
+        moves << new_position
+        position = new_position
+      end
+    end
+    moves
+  end
+
+  def attack_moves(dx, dy)
+    moves = []
+    x, y = self.position
+    x, y = x+dx, y+dy
+    position = [x, y]
+    if @board[position].color != self.color && @board.valid_position?(position)
+      moves << position
+    end
+    moves
+  end
+
   
 end
