23d38a84da00041199b523ea9afcabc9e2c7f1fd
Arash <arash.mh01@gmail.com>
2019-01-21 19:13:29 -0800

    Check for valid moves

--- chess/piece.rb
+++ chess/piece.rb
@@ -12,15 +12,16 @@ class Piece
     'P'.colorize(color)
   end
 
-  def valid_moves
+  def valid_moves(end_pos)
+    valid_moves = []
+    @board.move_piece!(position, end_pos) unless move_into_check?(end_pos)
     
   end
 
   def move_into_check?(end_pos)
     new_board = board.dup
     new_board.move_piece!(position, end_pos)
-    
-
+    new_board.in_check?(color)
   end
 
   
