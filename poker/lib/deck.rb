ee25a0328bc068fcb71435b0fd79c5ed7ce4fa4a
Arash <arash.mh01@gmail.com>
2019-01-25 15:36:19 -0800

    Pass specs for deck class

--- poker/lib/deck.rb
+++ poker/lib/deck.rb
@@ -1,5 +1,26 @@
+require_relative 'card'
 class Deck
+  RANKS = ['A', '2', '3', '4', '5', '6', '7',
+    '8', '9', '10', 'J', 'Q', 'K']
+  SUITS = ['diamonds', 'clubs', 'hearts', 'spades']
+
+  attr_reader :cards
   def initialize
+    @cards = generate_cards
+    # 52.times {|i| @cards << Card.new('5', 'clubs')}
     
   end
+
+  private 
+  def generate_cards
+    cards = []
+    SUITS.each do |suit|
+      RANKS.each do |rank|
+        cards << Card.new(rank, suit)
+      end
+    end
+    cards
+  end
+
+
 end
--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -1,5 +1,18 @@
 require "deck"
 
 describe Deck do
-  
+  subject(:deck) { Deck.new }
+
+  describe '#initialize' do 
+
+    it "generates a deck of 52 cards" do 
+      expect(deck.cards.count).to eq(52)
+    end
+
+    it "has all four suits and ranks in it" do 
+      expect(deck.cards[50].suit).to include('spades')
+     
+    end
+
+  end
 end
