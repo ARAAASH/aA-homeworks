04bfce6af14c51181b464004c4fdaf6b141663b6
Arash <arash.mh01@gmail.com>
2019-01-24 21:37:40 -0800

    Complete tests for two sum method

--- first_tdd/lib/tdd.rb
+++ first_tdd/lib/tdd.rb
@@ -1,3 +1,5 @@
+# require "byebug"
+
 def my_uniq(array)
   hash = Hash.new(0)
   array.each { |ele| hash[ele] += 1 }
@@ -8,6 +10,22 @@ def my_uniq(array)
   arr
 end
 
-def two_sum
+def two_sum(array)
+  res = []
+  array.each_with_index do |ele, i|
+    first_ele = array[i]
+    # byebug
+    j = i + 1
+    array[j..-1].each do |ele2|
+      if (first_ele + ele2) == 0
+        # byebug
+        res << [i, j]
+      end
+      j += 1
+    end
+  end
+  res
   
 end
+
+two_sum([-1, 0, 2, -2, 1])
--- first_tdd/spec/tdd_spec.rb
+++ first_tdd/spec/tdd_spec.rb
@@ -14,14 +14,24 @@ end
 
 describe "#two_sum" do
   subject(:array) { [-1, 0, 2, -2, 1] }
+  let (:ar) { [] }
+  let (:arr1) { [-4, 5, 4, 3, -4] }
+  let (:arr2) { [7, -7, -7, 4, 3, -4] }
 
   it "finds all pairs of positions where the elements at those positions sum to zero." do
     expect(two_sum(array)).to eq([[0, 4], [2, 3]])
   end
 
-  it "always outputs 2-d array" do 
-    expect(two_sum(array)[0]).not_to eq(nil) 
+  it "generates empty array for empty input" do
+    expect(two_sum(ar)).to eq([])
   end
 
-  it "generates empty array for empty input"
+  it "smaller first element, in the output, comes first" do
+    expect(two_sum(arr1)).to eq([[0,2], [2,4]])
+  end
+
+  it "outputs is sorted dictionary-wise" do
+    expect(two_sum(arr2)).to eq([[0,1], [0,2], [3,5]])
+  end
+
 end
