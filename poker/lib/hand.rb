455198a503870ca7484b986bfca2e954a79e4b6b
Arash <arash.mh01@gmail.com>
2019-01-25 16:59:34 -0800

    Pass tests for flush method

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -8,7 +8,19 @@ class Hand
   end
 
   def num_of_a_kind
-    
+    kinds = []
+    cards.each { |card| kinds << card.rank }
+    kinds.length - kinds.uniq.length + 1
+  end
+
+  def flush
+    suits = []
+    cards.each { |card| suits << card.suit }
+    if cards.uniq.length == 1
+      return true
+    else
+      return false
+    end
   end
 
 end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -12,14 +12,26 @@ describe Hand do
   end
   
   describe "#num_of_a_kind" do 
-  let(:card1) { double('card1', :rank => 'A') }
-  let(:card2) { double('card2', :rank => 'A') }
-  let(:card3) { double('card3', :rank => '5') }
-  let(:card4) { double('card4', :rank => 'A') }
-  subject(:hand) { Hand.new([card1, card2, card3, card4]) }
+    let(:card1) { double('card1', :rank => 'A') }
+    let(:card2) { double('card2', :rank => 'A') }
+    let(:card3) { double('card3', :rank => '5') }
+    let(:card4) { double('card4', :rank => 'A') }
+    subject(:hand) { Hand.new([card1, card2, card3, card4]) }
     it "counts number of a cards in a same rank" do
       expect(hand.num_of_a_kind).to eq(3)
     end
 
+  end
+
+  describe "#flush" do 
+    let(:card1) { double('card1', :suit => 'spades') }
+    let(:card2) { double('card2', :suit => 'spades') }
+    let(:card3) { double('card3', :suit => 'hearts') }
+    let(:card4) { double('card4', :suit => 'spades') }
+    let(:card5) { double('card5', :suit => 'spades') }
+    subject(:hand) { Hand.new([card1,card2,card3,card4,card5]) }
+    it "true if all the cards are in same suit" do 
+      expect(hand.flush).to be false  
+    end
   end
 end
