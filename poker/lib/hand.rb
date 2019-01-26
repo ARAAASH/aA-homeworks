99cef1d73a0fb09140e49d3e3a9d9ee3a1d5076b
Arash <arash.mh01@gmail.com>
2019-01-25 17:31:12 -0800

    Finish sequence and straight flush methods with their specs

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -7,13 +7,35 @@ class Hand
     @cards = cards
   end
 
+  def straight_flush?
+    sequence? && flush?
+  end
+
+  def pair?
+      if num_of_a_kind == 2
+  end
+  
+
+  def sequence?
+    card_values = []
+    cards.each { |card| card_values << card.get_value }
+    new_card_values = []
+    sort_values = card_values.sort
+    card_values.length.times { |i| new_card_values << sort_values[0]+i }
+    if sort_values == new_card_values
+      return true
+    else
+      return false
+    end
+  end
+
   def num_of_a_kind
     kinds = []
     cards.each { |card| kinds << card.rank }
     kinds.length - kinds.uniq.length + 1
   end
 
-  def flush
+  def flush?
     suits = []
     cards.each { |card| suits << card.suit }
     if cards.uniq.length == 1
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -1,13 +1,16 @@
 require "hand"
 
 describe Hand do 
-  let(:card1) { double('card1', :rank => 'A') }
-  let(:card2) { double('card2', :rank => 'A') }
-  subject(:hand) { Hand.new([card1, card2]) }
+  let(:card1) { double('card1', :rank => '2', :get_value => 2, :suit => 'spades') }
+    let(:card2) { double('card2', :rank => 'A', :get_value => 1, :suit => 'spades') }
+    let(:card3) { double('card3', :rank => '5', :get_value => 5, :suit => 'spades') }
+    let(:card4) { double('card4', :rank => '3', :get_value => 3, :suit => 'spades') }
+    let(:card5) { double('card5', :rank => '4', :get_value => 4, :suit => 'spades') }
+    subject(:hand) { Hand.new([card1,card2,card3,card4,card5]) }
   describe '#initialize' do 
     
     it 'takes an an array of cards' do
-      expect(hand.cards).to eq([card1, card2])
+      expect(hand.cards).not_to eq([card1, card2])
     end
   end
   
@@ -23,7 +26,7 @@ describe Hand do
 
   end
 
-  describe "#flush" do 
+  describe "#flush?" do 
     let(:card1) { double('card1', :suit => 'spades') }
     let(:card2) { double('card2', :suit => 'spades') }
     let(:card3) { double('card3', :suit => 'hearts') }
@@ -31,7 +34,21 @@ describe Hand do
     let(:card5) { double('card5', :suit => 'spades') }
     subject(:hand) { Hand.new([card1,card2,card3,card4,card5]) }
     it "true if all the cards are in same suit" do 
-      expect(hand.flush).to be false  
+      expect(hand.flush?).to be false  
     end
   end
+
+  describe "#sequence?" do 
+    it "returns true if all the cards are in sequence" do
+
+      expect(hand.sequence?).to be true
+    end
+  end
+
+  describe "#straight_flush?" do 
+    it "returns true if hand is straight flush" do 
+      expect(hand.straight_flush?).to be false
+    end
+  end
+
 end
