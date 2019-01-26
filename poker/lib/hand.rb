9ed649bc52a107f4537f7de9c8aba3c7763bc977
Arash <arash.mh01@gmail.com>
2019-01-25 18:32:08 -0800

    Finish hand ranking in hand class

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -12,9 +12,74 @@ class Hand
   end
 
   def pair?
-      if num_of_a_kind == 2
+    num_of_a_kind == 2 && !flush? && !full_house?
   end
-  
+
+  def four_of_a_kind?
+    num_of_a_kind == 4 
+  end
+
+  def three_of_a_kind?
+    num_of_a_kind == 3 && !flush? && !full_house?
+  end
+
+  def straight?
+    sequence? && !flush? 
+  end
+
+  def flush?
+    suits = []
+    cards.each { |card| suits << card.suit }
+    if suits.uniq.length == 1
+      return true
+    else
+      return false
+    end
+  end
+
+  def full_house?
+    card_values = []
+    cards.each { |card| card_values << card.get_value }
+    uniq_cards = card_values.sort.uniq
+    if uniq_cards.length == 2
+      count_cards1 = card_values.count(uniq_cards[0])
+      count_cards2 = card_values.count(uniq_cards[1])
+      if (count_cards1 == 2 && count_cards2 == 3) || (count_cards1 == 3 && count_cards2 == 2)
+        return true
+      else
+        return false
+      end  
+    else
+      return false
+    end
+
+  end
+
+  def two_pairs?
+    card_values = []
+    cards.each { |card| card_values << card.get_value }
+    uniq_cards = card_values.sort.uniq
+   
+    if uniq_cards.length == 3
+
+      count_cards1 = card_values.count(uniq_cards[0])
+   
+      count_cards2 = card_values.count(uniq_cards[1])
+   
+      count_cards3 = card_values.count(uniq_cards[2])
+    
+      if (count_cards1 == 2 && count_cards2 == 2) || (count_cards1 == 2 && count_cards3 == 2) || (count_cards2 == 2 && count_cards3 == 2)
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
+
+  end
+
+
 
   def sequence?
     card_values = []
@@ -33,16 +98,6 @@ class Hand
     kinds = []
     cards.each { |card| kinds << card.rank }
     kinds.length - kinds.uniq.length + 1
-  end
-
-  def flush?
-    suits = []
-    cards.each { |card| suits << card.suit }
-    if cards.uniq.length == 1
-      return true
-    else
-      return false
-    end
   end
 
 end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -2,11 +2,12 @@ require "hand"
 
 describe Hand do 
   let(:card1) { double('card1', :rank => '2', :get_value => 2, :suit => 'spades') }
-    let(:card2) { double('card2', :rank => 'A', :get_value => 1, :suit => 'spades') }
-    let(:card3) { double('card3', :rank => '5', :get_value => 5, :suit => 'spades') }
-    let(:card4) { double('card4', :rank => '3', :get_value => 3, :suit => 'spades') }
-    let(:card5) { double('card5', :rank => '4', :get_value => 4, :suit => 'spades') }
-    subject(:hand) { Hand.new([card1,card2,card3,card4,card5]) }
+  let(:card2) { double('card2', :rank => 'A', :get_value => 1, :suit => 'spades') }
+  let(:card3) { double('card3', :rank => '5', :get_value => 5, :suit => 'spades') }
+  let(:card4) { double('card4', :rank => '3', :get_value => 3, :suit => 'spades') }
+  let(:card5) { double('card5', :rank => '4', :get_value => 4, :suit => 'spades') }
+  subject(:hand) { Hand.new([card1,card2,card3,card4,card5]) }
+  
   describe '#initialize' do 
     
     it 'takes an an array of cards' do
@@ -27,14 +28,14 @@ describe Hand do
   end
 
   describe "#flush?" do 
-    let(:card1) { double('card1', :suit => 'spades') }
-    let(:card2) { double('card2', :suit => 'spades') }
-    let(:card3) { double('card3', :suit => 'hearts') }
-    let(:card4) { double('card4', :suit => 'spades') }
-    let(:card5) { double('card5', :suit => 'spades') }
-    subject(:hand) { Hand.new([card1,card2,card3,card4,card5]) }
+    # let(:card1) { double('card1', :rank => 'A', :suit => 'spades') }
+    # let(:card2) { double('card2', :rank => 'A', :suit => 'spades') }
+    # let(:card3) { double('card3', :rank => 'A', :suit => 'spades') }
+    # let(:card4) { double('card4', :rank => '3', :suit => 'spades') }
+    # let(:card5) { double('card5', :rank => 'A', :suit => 'spades') }
+    # subject(:hand) { Hand.new([card1,card2,card3,card4,card5]) }
     it "true if all the cards are in same suit" do 
-      expect(hand.flush?).to be false  
+      expect(hand.flush?).to be true  
     end
   end
 
@@ -46,8 +47,40 @@ describe Hand do
   end
 
   describe "#straight_flush?" do 
+
     it "returns true if hand is straight flush" do 
-      expect(hand.straight_flush?).to be false
+      expect(hand.straight_flush?).to be true
+    end
+  end
+
+  describe "#pair?" do 
+    it "returns true if the hand is pair" do 
+      expect(hand.pair?).to be false
+    end
+  end
+
+  describe "#two_pairs?" do 
+    let(:card1) { double('card1', :rank => 'A', :get_value => 1, :suit => 'spades') }
+    let(:card2) { double('card2', :rank => 'A', :get_value => 1, :suit => 'spades') }
+    let(:card3) { double('card3', :rank => '3', :get_value => 3, :suit => 'spades') }
+    let(:card4) { double('card4', :rank => '3', :get_value => 3, :suit => 'clubs') }
+    let(:card5) { double('card5', :rank => '5', :get_value => 5, :suit => 'spades') }
+    subject(:hand) { Hand.new([card1,card2,card3,card4,card5]) }
+    it "returns true if hand has 2 pairs and it's not flush" do 
+      expect(hand.two_pairs?).to be true
+    end
+  end
+
+  describe "#full_house?" do 
+  let(:card1) { double('card1', :rank => 'A', :get_value => 1, :suit => 'spades') }
+    let(:card2) { double('card2', :rank => '3', :get_value => 3, :suit => 'spades') }
+    let(:card3) { double('card3', :rank => '3', :get_value => 3, :suit => 'spades') }
+    let(:card4) { double('card4', :rank => '3', :get_value => 3, :suit => 'spades') }
+    let(:card5) { double('card5', :rank => 'A', :get_value => 1, :suit => 'spades') }
+    subject(:hand) { Hand.new([card1,card2,card3,card4,card5]) }
+
+    it "returns true if hands 3_of_a_kind and a pair" do 
+      expect(hand.full_house?).to be true
     end
   end
 
