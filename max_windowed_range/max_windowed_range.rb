055e475734b2f0be6244eff0093413ea6b7f0b49
Arash <arash.mh01@gmail.com>
2019-01-27 14:38:47 -0800

    Implement queue data structure

--- max_windowed_range/max_windowed_range.rb
+++ max_windowed_range/max_windowed_range.rb
@@ -13,4 +13,31 @@ def max_windowed_range(array, window_size)
   maximums.max
 end
 
+class MyQueue
+  def initialize
+    @store = []
+  end
+  
+  def size
+    @store.length
+  end
+
+  def empty?
+    @store.size == 0
+  end
+
+  def peek
+    @store[0]
+  end
+
+  def enqueue(ele)
+    @store << ele
+  end
+
+  def dequeue
+    @store.delete_at(0)
+  end
+
+end
+
 p max_windowed_range([1, 0, 2, 5, 4, 8], 2)
