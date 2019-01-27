e86cba2595c0b90fd188177d04600a40c1d251ec
Arash <arash.mh01@gmail.com>
2019-01-27 13:19:44 -0800

    Complete two sum methods

--- two_sum/two_sum.rb
+++ two_sum/two_sum.rb
@@ -1,3 +1,5 @@
+
+# O(n^2)
 def bad_two_sum?(arr, target)
 
   arr.each_with_index do |ele, i|
@@ -8,7 +10,40 @@ def bad_two_sum?(arr, target)
     end
   end
   false
-  
 end
+
+# O(n log n)
+def okay_two_sum?(arr, target)
+  arr_sorted = arr.sort
+  l = 0
+  r = arr_sorted.length - 1
+  while l < r
+    if arr_sorted[l] + arr_sorted[r] == target
+      return true
+    elsif arr_sorted[l] + arr_sorted[r] < target
+      l += 1
+    else
+      r -= 1
+    end
+  end
+  false
+end
+
+def two_sum?(arr, target)
+  hash = Hash.new(0)
+
+  arr.each do |ele|
+    if hash.include?(target - ele)
+      return true
+    else
+      hash[ele] += 1
+    end
+  end
+  false
+
+end
+
+
+
 arr = [0, 1, 5, 7]
-p bad_two_sum?(arr, 16) 
+p two_sum?(arr, 8) 
