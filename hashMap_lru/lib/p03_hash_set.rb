2985e6f12e185c3e1e759d7304e632f8865712ab
Arash <arash.mh01@gmail.com>
2019-01-28 21:43:34 -0800

    Finish HashSet class

--- hashMap_lru/lib/p03_hash_set.rb
+++ hashMap_lru/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,18 +7,33 @@ class HashSet
   end
 
   def insert(key)
+    if !include?(key)
+      if @count < 8
+        self[key] << key
+      else
+        resize!
+        self[key] << key
+      end
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +41,12 @@ class HashSet
   end
 
   def resize!
+    store1 = Array.new(2 * num_buckets) {Array.new}
+    @store += store1
+    @store[0...num_buckets/2].each do |bucket|
+      bucket.length.times do |ele|
+        self[ele] << ele
+      end
+    end
   end
 end
