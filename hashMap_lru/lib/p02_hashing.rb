e7dd42a897e1d96d50e129ceabebed42a48edef8
Arash <arash.mh01@gmail.com>
2019-01-28 20:25:00 -0800

    Build a hash function by using ascii and xor

--- hashMap_lru/lib/p02_hashing.rb
+++ hashMap_lru/lib/p02_hashing.rb
@@ -4,6 +4,24 @@ end
 
 class Array
   def hash
+    sum_ascii = 0
+    self.each_with_index do |ele, i|
+      if ele.is_a?(String)
+        sum_ascii += get_ascii(ele, i)
+      else
+        sum_ascii += get_ascii(ele.to_s, i)
+      end
+    end
+
+    sum_ascii ^ 255
+  end
+
+  def get_ascii(str, i)
+    ascii_num = 0
+    str.each_char do |c|
+      ascii_num += c.ord * i
+    end
+    ascii_num
   end
 end
 
