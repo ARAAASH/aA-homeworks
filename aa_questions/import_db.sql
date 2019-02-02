12fdff4f8d80b0aab48b28e7ea1925acc4c09ce5
Arash <arash.mh01@gmail.com>
2019-02-01 16:46:28 -0800

    Create tables for the database

---
+++ aa_questions/import_db.sql
@@ -0,0 +1,62 @@
+
+PRAGMA foreign_keys = ON;  -- turn on the foreign key constraints to ensure data integrity
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  body TEXT,
+  question_id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  question_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE tags (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE question_tags (
+  id INTEGER PRIMARY KEY,
+  tag_id INTEGER NOT NULL,
+
+  FOREIGN KEY (tag_id) REFERENCES tags(id)
+);
+
+
+
+
+
