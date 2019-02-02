0ae7db7a4660160c9b97d2499094fdc73957c75a
Arash <arash.mh01@gmail.com>
2019-02-02 13:29:13 -0800

    Finish question follows class for question follows table

---
+++ aa_questions/question_follow.rb
@@ -0,0 +1,19 @@
+require_relative 'questions_database'
+
+class QuestionFollow
+  
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM question_follows
+      WHERE id = ?
+    SQL
+    QuestionFollow.new(data.first)
+  end
+  attr_accessor :id, :user_id, :question_id
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+end
--- aa_questions/user.rb
+++ aa_questions/user.rb
@@ -11,10 +11,21 @@ class User
     User.new(data.first)
   end
 
+  def self.find_by_name(fname, lname)
+    data = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT *
+      FROM users
+      WHERE 
+        fname = ? AND lname = ?
+    SQL
+    User.new(data.first)
+  end
+
   attr_accessor :id, :fname, :lname
   def initialize(options)
     @id = options['id']
     @fname = options['fname']
     @lname = options['lname']
   end
+
 end
