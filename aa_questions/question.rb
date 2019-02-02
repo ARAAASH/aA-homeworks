e673b51c7f5b014e34e51b02d28c2b62e0e31592
Arash <arash.mh01@gmail.com>
2019-02-02 12:15:07 -0800

    Finish question class for questions table

--- aa_questions/question.rb
+++ aa_questions/question.rb
@@ -1,12 +1,34 @@
-require 'sqlite3'
-require 'singleton'
+require_relative 'questions_database'
 
-class QuestionsDatabase < SQLite3::Database
-  include Singleton
-  
-  def initialize
-    super('questions.db')
-    self.type_translation = true
-    self.results_as_hash = true
+class Question
+
+
+  def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM questions
+      WHERE id = ?
+    SQL
+    Question.new(data.first)
+    # data
+  end
+
+  attr_accessor :id, :title, :body, :author_id
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+
+  def create
+    raise "#{self} is already in the table" if @id
+    QuestionsDatabase.instance.execute(<<-SQL, title, body, author_id)
+      INSERT INTO
+        questions(title, body, author_id)
+      VALUES
+        (?, ?, ?)
+    SQL
+    self.id = QuestionsDatabase.instance.last_insert_row_id
   end
 end
