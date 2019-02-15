12dc30ffee8f9f5cd26352e67f722b86a1a245f4
Arash <arash.mh01@gmail.com>
2019-02-15 15:21:50 -0800

    Complete intialize and model class methods

--- active_record_lite/lib/03_associatable.rb
+++ active_record_lite/lib/03_associatable.rb
@@ -10,23 +10,40 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    if options.empty?
+      @foreign_key = (name.to_s.camelcase + 'Id').underscore.to_sym
+      @primary_key = :id
+      @class_name = name.to_s.camelcase
+    else
+      @foreign_key = options[:foreign_key] unless options[:foreign_key].nil?
+      @class_name = options[:class_name] unless options[:class_name].nil?
+      @primary_key = options[:primary_key] unless options[:primary_key].nil?
+    end
+
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    if options.empty?
+      @primary_key = :id
+      @foreign_key = (self_class_name + 'Id').underscore.to_sym
+      @class_name = name.singularize.camelcase
+    else
+      @primary_key = options[:primary_key] unless options[:primary_key].nil?
+      @foreign_key = options[:foreign_key] unless options[:foreign_key].nil?
+      @class_name = options[:class_name] unless options[:class_name].nil?
+    end
   end
 end
 
