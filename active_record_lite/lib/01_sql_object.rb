d3deb472f514d77b7f003c1e647a0eaa6d81dae0
Arash <arash.mh01@gmail.com>
2019-02-15 12:46:54 -0800

    Implement instance method insert

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -82,11 +82,23 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |attr| self.send(attr) }
   end
 
   def insert
-    # ...
+    columns = self.class.columns.drop(1)
+    col_names = columns.map(&:to_sym).join(', ')
+    question_marks = (['?']*columns.count).join(', ')
+
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.send("id=", DBConnection.last_insert_row_id)
+
   end
 
   def update
