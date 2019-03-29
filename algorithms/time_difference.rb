4219848480c6a80813b8f75da9796238e435d2f8
Arash <arash.mh01@gmail.com>
2019-03-29 14:00:34 -0700

    Time difference alg

--- algorithms/time_difference.rb
+++ algorithms/time_difference.rb
@@ -32,18 +32,46 @@ end
 # list = [2, 3, -6, 7, -6, 7]
 # largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
 
+# O(n^3)
 def largest_contiguous_subsum(arr)
+  sub = []
+  0.upto(arr.length-1) do |i|
+    (i).upto(arr.length-1) do |j|
+      sub << arr[i..j]
+    end
+  end
+  sums = []
+  sub.each do |a|
+    sums << a.reduce(:+)
+  end
+  sums.max
+end
+
+def largest_contiguous_subsum1(arr)
   
 end
 
+def remove_duplicates(nums)
+  return 0 if nums.size < 1
+  i = 0
+  j = 1
+  while j <= nums.size-1
+    if nums[i] == nums[j]
+      j += 1
+    else
+      
+      i += 1
+      nums[i], nums[j] = nums[j], nums[i]
+      j += 1
+    
+    end
+  end
+  p nums  
+  return i+1
+end
 
+# p largest_contiguous_subsum([-5, -1, -3])
 
-
-
-
-
-
-
-
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)
+p remove_duplicates([1,1,2,2,2,2,2,3])
